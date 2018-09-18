ea874ff6908aa1730d0d8e76ab8fa9fbf02c0be2
zachshult <zachshult12345@gmail.com>
2018-09-17 23:50:31 -0500

    Updated Models
    - Upadeted models.js

---
+++ models/burger.js
@@ -0,0 +1,15 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+
+
+
+
+
+
+
+
+// Export the database functions for the controller (catsController.js).
+module.exports = cat;
+
+
